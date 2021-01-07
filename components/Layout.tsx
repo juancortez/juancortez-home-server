8809cf14fa630d331f4f480db3f5944b591d288f
Juan Cortez <juan_cortez@utexas.edu>
2021-01-07 12:16:45 -0600

    setting up font and modifying home page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,25 @@
 import React, { ReactNode } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
+import { createGlobalStyle } from 'styled-components'
+import styled from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Noto Sans', sans-serif;
+    font-display: swap;
+    color: #111;
+  }
+`;
+
+const StyledMain = styled.main`
+  padding-left: 3vw;
+  padding-right: 3vw;
+`;
 
 type Props = {
   children?: ReactNode
@@ -9,15 +28,18 @@ type Props = {
 
 const Layout = ({ children, title = 'This is the default title' }: Props) => (
   <div>
+  <GlobalStyle/>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"></link>
     </Head>
-    <header>
+    {/* <header>
       <nav>
         <Link href="/">
-          <a>Home</a>
+          <a>Work</a>
         </Link>{' '}
         |{' '}
         <Link href="/about">
@@ -25,16 +47,17 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
         </Link>{' '}
         |{' '}
         <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
+          <a>Resume</a>
+        </Link>
       </nav>
-    </header>
-    {children}
-    <footer>
+    </header> */}
+    <StyledMain>
+      {children}
+    </StyledMain>
+    {/* <footer>
       <hr />
       <span>I'm here to stay (Footer)</span>
-    </footer>
+    </footer> */}
   </div>
 )
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,165 @@
-import Link from 'next/link'
 import Layout from '../components/Layout'
+import styled from 'styled-components';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const StyledHeaderSection = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+
+  margin-top: 10vh;
+  margin-bottom: 10vh;
+`;
+
+const StyledArticle = styled.article`
+  max-width: 67vw;
+`;
+
+const StyledNameHeader = styled.h1`
+  font-size: 42px;
+  font-weight: bold;
+`;
+
+const StyledArticleText = styled.h2`
+  font-size: 36px;
+  font-weight: bold;
+`;
+
+const StyledBlockQuote = styled.blockquote`
+  font-style: italic;
+`;
+
+const StyledContentSection = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-content: center;
+  flex-wrap: wrap;
+`;
+
+const CompanyOverlay = styled.div`
+  transition: .5s ease;
+  opacity: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  text-align: center;
+`;
+
+const StyledCompanyImage = styled.img`
+  max-height: 500px;
+  max-width: 300px;
+`;
+
+
+const StyledCompanyArticle = styled.article`
+  opacity: 1;
+  position: relative;
+  min-width: 450px;
+  max-width: 500px;
+  margin: auto;
+  transition: background-color 140ms cubic-bezier(.33,0,.2,1);
+  padding: 16px;
+
+  &:hover ${StyledCompanyImage} {
+    opacity: 0.1;
+  }
+
+  &:hover ${CompanyOverlay} {
+    opacity: 1;
+  }
+`;
+
+const StyledFigure = styled.figure`
+  flex-direction: column;
+  align-items: center;
+  margin: 0px;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  color: #111;
+`;
+
+
+const StyledCompanyLink = styled.a`
+  text-decoration: none;
+`;
+
+const StyledFigCaption = styled.figcaption`
+  color: black;
+  font-size: 40px;
+  padding: 16px 32px;
+`;
+
+interface CompanyMetadata {
+  linkUrl: string;
+  imageUrl: string;
+  company: string;
+}
+
+const companies: CompanyMetadata[] = [
+  {
+    linkUrl: '/amazon',
+    imageUrl: '/images/Amazon.svg',
+    company: 'Amazon',
+  },
+  {
+    linkUrl: '/rigup',
+    imageUrl: '/images/RigUp.svg',
+    company: 'RigUp',
+  },
+  {
+  linkUrl: '/microsoft',
+  imageUrl: '/images/Microsoft.svg',
+  company: 'Microsoft'
+},
+{
+  linkUrl: '/postmates',
+  imageUrl: '/images/Postmates.svg',
+  company: 'Postmates',
+},
+{
+  linkUrl: '/ibm',
+  imageUrl: '/images/IBM.svg',
+  company: 'IBM',
+},
+];
+
+const IndexPage = () => {
+  return (
+  <Layout title="Juan Cortez">
+      <StyledHeaderSection>
+        <StyledNameHeader>Hi. I'm Juan Cortez.</StyledNameHeader>
+
+        <StyledArticle>
+          <StyledArticleText>Empathy towards end users has always been my greatest trait.</StyledArticleText>
+          <StyledBlockQuote>"When people talk, listen completely. Most people never listen." - Ernest Hemingway</StyledBlockQuote>
+        </StyledArticle>
+      </StyledHeaderSection>
+
+      <StyledContentSection>
+        {companies.map((c: CompanyMetadata) => {
+          return (
+            <StyledCompanyArticle key={c.company}>
+                <StyledCompanyLink href={c.linkUrl}>
+                  <StyledFigure>
+                    <StyledCompanyImage src={c.imageUrl}/>
+                    <CompanyOverlay>
+                      <StyledFigCaption>{c.company}</StyledFigCaption>
+                    </CompanyOverlay>
+                  </StyledFigure>
+                </StyledCompanyLink>
+            </StyledCompanyArticle>
+          )
+        })}
+      </StyledContentSection>
+    </Layout>
+  )
+}
+
 
 export default IndexPage
